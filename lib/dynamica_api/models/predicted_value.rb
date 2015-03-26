90b23f1ae2bacd12bfdb13f4eb067ea4f931deac
Alexander Borovykh <immaculate.pine@gmail.com>
2015-03-26 12:04:59 +0100

    Timestamp removed from PredictedValue model

--- lib/dynamica_api/models/predicted_value.rb
+++ lib/dynamica_api/models/predicted_value.rb
@@ -2,7 +2,6 @@ module DynamicaAPI
   class PredictedValue < Base
     property :from
     property :to
-    property :timestamp
     property :value
     property :predicted
     attr_accessor :forecast_line
